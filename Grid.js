2fd1bc3c8ddc6ea15e706dab4ad752f71e2aa649
saurabh <saurabhkedar@gmail.com>
2022-03-29 02:23:38 +0530

    completed the module for the Grid structure

--- Grid.js
+++ Grid.js
@@ -6,10 +6,39 @@ const cellGap = 2
 //   --cell-size: 20vmin;
 //   --cell-gap: 2vmin;
 export default class Grid {
+  #cells
   constructor(gridElement) {
     gridElement.style.setProperty('--grid-size', gridSize)
     gridElement.style.setProperty('--cell-size', `${cellSize}vmin`)
     gridElement.style.setProperty('--cell-gap', `${cellGap}vmin`)
-    createCellElement(gridElement)
+    this.#cells = createCellElement(gridElement).map((cellElement, index) => {
+      return new Cell(
+        cellElement,
+        index % gridSize,
+        Math.floor(index / gridSize)
+      )
+    })
   }
+}
+
+class Cell {
+  #cellElement
+  #x
+  #y
+  constructor(cellElement, x, y) {
+    this.#cellElement = cellElement
+    this.#x = x
+    this.#y = y
+  }
+}
+
+function createCellElement(gridElement) {
+  const cells = []
+  for (let i = 0; i < gridSize ** 2; i++) {
+    const cell = document.createElement('div')
+    cell.classList.add('cell')
+    cells.push(cell)
+    gridElement.appendChild(cell)
+  }
+  return cells
 }
