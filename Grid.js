6f0d7cb86933e5e5fbc8c987c3ea06cd7d3f8e1a
saurabh <saurabhkedar@gmail.com>
2022-03-30 23:58:59 +0530

    added verticle slide movements phase 1

--- Grid.js
+++ Grid.js
@@ -21,6 +21,14 @@ export default class Grid {
     })
   }
 
+  get cellsByColumn() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.x] = cellGrid[cell.x] || []
+      cellGrid[cell.x][cell.y] = cell
+      return cellGrid
+    }, [])
+  }
+
   get #emptyCells() {
     return this.#cells.filter((cell) => cell.tile == null)
   }
@@ -36,15 +44,46 @@ class Cell {
   #x
   #y
   #tile
-
+  #mergeTile
   constructor(cellElement, x, y) {
     this.#cellElement = cellElement
     this.#x = x
     this.#y = y
   }
 
+  get x() {
+    return this.#x
+  }
+  get y() {
+    return this.#y
+  }
+
   get tile() {
     return this.#tile
+  }
+  set tile(value) {
+    this.#tile = value
+    if (value == null) return
+    this.#tile.x = this.#x
+    this.#tile.y = this.#y
+  }
+
+  get mergeTile() {
+    return this.#mergeTile
+  }
+
+  set mergeTile(value) {
+    this.#mergeTile = value
+    if (value == null) return
+    this.#mergeTile.x = this.#x
+    this.#mergeTile.y = this.#y
+  }
+
+  canAccept(tile) {
+    return (
+      this.tile == null ||
+      (this.mergeTile == null && this.tile.value === tile.value)
+    )
   }
 }
 
--- app.js
+++ app.js
@@ -1,8 +1,66 @@
 import Grid from './Grid.js'
+import Tile from './Tile.js'
 
 const gameBoard = document.getElementById('game-board')
 const grid = new Grid(gameBoard)
 
-console.log(grid.randomEmptyCell())
-// grid.randomEmptyCell().tile = new Tile(gameBoard)
-// grid.randomEmptyCell().tile = new Tile(gameBoard)
+// console.log(grid.randomEmptyCell())
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+setupInput()
+
+console.log(grid.cellsByColumn)
+
+function setupInput() {
+  window.addEventListener('keydown', handleInput, { once: true })
+}
+
+function handleInput(e) {
+  console.log(e.key)
+  switch (e.key) {
+    case 'ArrowUp':
+      moveUp()
+      break
+    case 'ArrowDown':
+      moveDown()
+      break
+    case 'ArrowRight':
+      moveRight()
+      break
+    case 'ArrowLeft':
+      moveLeft()
+      break
+    default:
+      setupInput()
+      return
+  }
+  setupInput()
+}
+
+function moveUp() {
+  slideTiles(grid.cellsByColumn)
+}
+
+function slideTiles(cells) {
+  cells.forEach((group) => {
+    for (let i = 1; i < group.length; i++) {
+      const cell = group[i]
+      if (cell.tile == null) continue
+      let lastValidCell
+      for (j = i - 1; j >= 0; j--) {
+        const moveToCell = group[j]
+        if (!moveToCell.canAccept(cell.tile)) break
+        lastValidCell = moveToCell
+      }
+
+      if (lastValidCell != null) {
+        if (lastValidCell.tile != null) {
+          lastValidCell.mergeTile = cell.tile
+        } else {
+          lastValidCell.tile = cell.tile
+        }
+        cell.tile = null
+      }
+    }
+  })
+}
